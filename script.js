e6b50140689be3fb53aac8022da15c899bc8c17c
Samir Vargas <galeriacasagrande224@gmail.com>
2024-09-15 17:58:16 -0500

    Create exit button of the hamburger menu. 0.4

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ function showBar() {
         bar = document.getElementById("barmenu");
         bar.style.display = "block";
         clickBar = true;
+        exitbt = document.getElementById("CloseOpcID");
+        exitbt.style.display = "block";
     }else{
         bar = document.getElementById("barmenu");
         bar.style.display = "none";
         clickBar = false;
     }
+}
+function ExitButton(){
+    exitbt = document.getElementById("CloseOpcID");
+    exitbt.style.display = "none";
+    bar = document.getElementById("barmenu");
+    bar.style.display = "none";
+    clickBar = false;
 }
